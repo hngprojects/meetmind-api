409e7a4fd6240b4b0a7c0d28be057aa9001dbd5e
Afebu Balogun <balogunafebu22@gmail.com>
2026-05-06 15:54:25 +0100

    feat: Add custom exception handlers for HTTP and validation errors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from app.api.v1.router import api_router
 from app.core.config import settings
+from app.schemas.auth import ErrorResponse
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -11,3 +14,28 @@ app.include_router(api_router, prefix=settings.API_V1_PREFIX)
 @app.get("/")
 def root() -> dict[str, str]:
     return {"message": f"{settings.PROJECT_NAME} is running"}
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=ErrorResponse(
+            status_code=exc.status_code,
+            message=exc.detail
+        ).model_dump()
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    first_error = errors[0]
+    field_name = " -> ".join(str(loc) for loc in first_error["loc"])
+    custom_message = f"Invalid input for {field_name}: {first_error['msg']}"
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+        content=ErrorResponse(
+            status_code=422,
+            message=custom_message
+        ).model_dump()
+    )
